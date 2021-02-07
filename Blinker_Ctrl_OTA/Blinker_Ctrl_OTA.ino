d7e236a839ea1cc1318c3ebb2c20d0ad23a9011c
Dragon-Keeper <44353148+Dragon-Keeper@users.noreply.github.com>
2021-02-08 01:06:00 +0800

    增加按键反馈和NFC开门与Blinker联系代码

--- Blinker_Ctrl_OTA/Blinker_Ctrl_OTA.ino
+++ Blinker_Ctrl_OTA/Blinker_Ctrl_OTA.ino
@@ -15,15 +15,18 @@ int count = 0;
 int count2 = 0;
 bool WIFI_Status = true;
 char auth[] = "8520021e8f7c";  //你的设备key
-int GPIO = 0;//定义GPIO口用于控制继电器
+int GPIO = 0; //定义GPIO 口用于控制继电器
 int GPIO1 = 2;//定义GPIO1口用于控制是否开启烧录模式
+int GPIO2 = 3;//定义GPIO2口用于控制是否开门
 int flag = 0;
 int ledPin = 2; //通过闪烁的快慢提示是否连上WiFi（未连上：快速闪烁；已连上：3秒一次）
 #define BUTTON_1 "Light_Key1"
 #define BUTTON_2 "BurnKey"
+#define BUTTON_3 "DoorKey"
 
 BlinkerButton Button1("Light_Key1");//这里需要根据自己在BLINKER里面设置的名字进行更改
 BlinkerButton Button2("BurnKey");//这里需要根据自己在BLINKER里面设置的名字进行更改
+BlinkerButton Button3("DoorKey");//这里需要根据自己在BLINKER里面设置的名字进行更改
 
 void smartConfig()//配网函数
 {
@@ -94,24 +97,48 @@ void WIFI_Init()
   //Blinker.delay(2000);
 }
 
-void button1_callback(const String & state)
-{
-  BLINKER_LOG("get Light_button state: ", state);
-  Serial.print("get Light_button state: ");
-  Serial.println(state);
-  digitalWrite(GPIO, !digitalRead(GPIO));
-  Blinker.vibrate(); //震动反馈
+// 控制灯的函数
+void button1_callback(const String & state) {
+  BLINKER_LOG("1#灯，状态: ", state);
+  if (state == "on") {
+    digitalWrite(GPIO, HIGH);
+    //Button1.icon("far fa-upload");
+    Button1.color("#FF0000");   //1#按钮按下时，app按键颜色状态显示是红色
+    // 反馈开关状态
+    Button1.text("已开灯");
+    Button1.print("on");
+    BLINKER_LOG ("1#灯已开灯on");  //串口打印
+  } else if (state == "off") {
+    digitalWrite(GPIO, LOW);
+    //Button1.icon("far fa-upload");
+    Button1.color("#000000");  //2#按钮没有按下时，app按键颜色状态显示是黑色
+    // 反馈开关状态
+    Button1.text("已关灯");
+    Button1.print("off");
+    BLINKER_LOG ("1#灯已关灯off");
+  }
 }
-
-void button2_callback(const String & state)
-{
-  BLINKER_LOG("get OTA_button state: ", state);
-  Serial.print("get OTA_button state: ");
-  Serial.println(state);
-  digitalWrite(GPIO1, !digitalRead(GPIO1));
+// 控制OTA的函数
+void button2_callback(const String & state) {
+  BLINKER_LOG("OTA，状态: ", state);
+  if (state == "on") {
+    digitalWrite(GPIO1, LOW);
+    //Button1.icon("far fa-door-closed");
+    Button2.color("#FF0000");   //2#按钮按下时，app按键颜色状态显示是红色
+    // 反馈开关状态
+    Button2.text("开OTA");
+    Button2.print("on");
+    BLINKER_LOG ("OTA开启");  //串口打印
+  } else if (state == "off") {
+    digitalWrite(GPIO1, HIGH);
+    //Button1.icon("far fa-door-open");
+    Button2.color("#000000");  //2#按钮没有按下时，app按键颜色状态显示是黑色
+    // 反馈开关状态
+    Button2.text("关OTA");
+    Button2.print("off");
+    BLINKER_LOG ("OTA关闭");
+  }
   int n = digitalRead(GPIO1);
-  Serial.println(n);
-  Blinker.vibrate(); //震动反馈
   if (n == HIGH)
   {
     flag = 0;
@@ -119,6 +146,27 @@ void button2_callback(const String & state)
   else
   {
     flag = 1;
+  }
+}
+// 控制门锁的函数
+void button3_callback(const String & state) {
+  BLINKER_LOG("门锁，状态: ", state);
+  if (state == "on") {
+    digitalWrite(GPIO2, HIGH);
+    //Button3.icon("far fa-upload");
+    Button3.color("#FF0000");   //1#按钮按下时，app按键颜色状态显示是红色
+    // 反馈开关状态
+    Button3.text("已开门");
+    Button3.print("on");
+    BLINKER_LOG ("已开门");  //串口打印
+  } else if (state == "off") {
+    digitalWrite(GPIO2, LOW);
+    //Button3.icon("far fa-upload");
+    Button3.color("#000000");  //2#按钮没有按下时，app按键颜色状态显示是黑色
+    // 反馈开关状态
+    Button3.text("已关门");
+    Button3.print("off");
+    BLINKER_LOG ("已关门");
   }
 }
 
@@ -154,20 +202,40 @@ void miotPowerState1(const String & state)
   }
 }
 
+void miotPowerState2(const String & state)
+{
+  BLINKER_LOG("need set Door state: ", state);
+
+  if (state == BLINKER_CMD_OFF) {//如果语音接收到是关闭烧录就设置GPIO口为高电平
+    digitalWrite(GPIO2, HIGH);
+    BlinkerMIOT.powerState("off");
+    BlinkerMIOT.print();
+  }
+  else if (state == BLINKER_CMD_ON) {
+    digitalWrite(GPIO2, LOW);
+    BlinkerMIOT.powerState("on");
+    BlinkerMIOT.print();
+  }
+}
+
 void setup() {
   Serial.begin(115200);
   pinMode(ledPin, OUTPUT);
   digitalWrite(ledPin, LOW);
   pinMode(GPIO, OUTPUT);
   pinMode(GPIO1, OUTPUT);
+  pinMode(GPIO2, OUTPUT);
   digitalWrite(GPIO, HIGH); //初始化，由于继电器是低电平触发。所以刚开始设为高电平
   digitalWrite(GPIO1, HIGH); //初始化，由于继电器是低电平触发。所以刚开始设为高电平
+  digitalWrite(GPIO2, HIGH); //初始化，由于继电器是低电平触发。所以刚开始设为高电平
   WIFI_Init();//调用WIFI函数
   Blinker.begin(auth, WiFi.SSID().c_str(), WiFi.psk().c_str());//运行blinker
   Button1.attach(button1_callback);
   Button2.attach(button2_callback);
+  Button3.attach(button3_callback);
   BlinkerMIOT.attachPowerState(miotPowerState);//这段代码一定要加，不加小爱同学控制不了,务必在回调函数中反馈该控制状态
   BlinkerMIOT.attachPowerState(miotPowerState1);//这段代码一定要加，不加小爱同学控制不了,务必在回调函数中反馈该控制状态
+  BlinkerMIOT.attachPowerState(miotPowerState2);//这段代码一定要加，不加小爱同学控制不了,务必在回调函数中反馈该控制状态
 
 }
 
