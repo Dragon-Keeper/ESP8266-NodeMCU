2895639ede41b1d1ce7adc48950add884d8621e4
Dragon-Keeper <44353148+Dragon-Keeper@users.noreply.github.com>
2021-03-17 19:22:15 +0800

    打开输入框随便输入什么触发信息返回，提交当前设备IP

---
+++ TEXT_WiFi/TEXT_WiFi.ino
@@ -0,0 +1,34 @@
+#define BLINKER_WIFI
+
+//启用非加密通信，获得更大的可用ram，不然8266ram不够就会重启。
+//目前仅可用于ESP8266，其他设备的RAM足以进行加密通信
+#define BLINKER_WITHOUT_SSL 
+
+#include <Blinker.h>
+
+char auth[] = "f5f144f4ede6";
+char ssid[] = "MERCURY_B8A4";
+char pswd[] = "3133xjzx100";
+
+#define TEXTE_1 "TextKey"
+BlinkerText Text1(TEXTE_1);
+
+void dataRead(const String & data)
+{
+  BLINKER_LOG("Blinker readString: ", data);
+  Blinker.vibrate();
+  Text1.print(WiFi.localIP().toString(),"/update");
+}
+
+void setup()
+{
+  Serial.begin(115200);
+  BLINKER_DEBUG.stream(Serial);
+  Blinker.begin(auth, ssid, pswd);
+  Blinker.attachData(dataRead);
+}
+
+void loop()
+{
+  Blinker.run();
+}
